bfcb33f0343c269adac767d3eabfa74e77e34060
ChuYaHsu <t199511137@gmail.com>
2022-12-12 10:58:20 +0800

    變數

--- practice/20221212-03-vars.php
+++ practice/20221212-03-vars.php
@@ -6,7 +6,7 @@ $c = 'ccc';
 // $my_age //snake
 // $myAge //camel
 // echo $age + $c . '<br>';  //若這裡發生錯誤，後面都不會執行
-echo $age + $b . '<br>';     //. => 為字串串接    
+echo $age + $b . '<br>';     //. -> 為字串串接    
 //php 之 +(加號) 為字值相加             
 echo $age + intval($c) . '<br>';   //打字串轉換為整數
 
