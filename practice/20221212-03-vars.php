954c1b195c94ad96d2f6dced5dac3b0e94f5b5cb
ChuYaHsu <t199511137@gmail.com>
2022-12-12 10:50:46 +0800

    變數

---
+++ practice/20221212-03-vars.php
@@ -0,0 +1,17 @@
+<?php
+
+$age = 12;           // $3 -> 第一個字元不可為數字
+$b = '5';
+$c = 'ccc';
+// $my_age //snake
+// $myAge //camel
+// echo $age + $c . '<br>';  //發生錯誤，後面都不會執行
+echo $age + $b . '<br>';     //. => 為字串串接    
+//php 之 +(加號) 為字值相加             
+echo $age + intval($c) . '<br>';   //打字串轉換為整數
+
+/*
+1.error
+2.warning
+3.notice
+*/
