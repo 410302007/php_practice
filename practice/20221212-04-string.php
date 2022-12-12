7850c835b358520b421dae4b5d37eff65f31fe09
ChuYaHsu <t199511137@gmail.com>
2022-12-12 11:37:42 +0800

    string

--- practice/20221212-04-string.php
+++ practice/20221212-04-string.php
@@ -2,7 +2,7 @@
 
 $name = 'David';
 
-echo "Hello $name <br> "; //若在雙引號有變數，就會把值帶進來
+echo "Hello $name <br> "; //若雙引號有變數，就會把值帶進來
 //塞變數-> 使用雙引號  
 echo "Hello {$name}123 <br>";   //7、8相同
 echo "Hello ${name}123 <br>";
