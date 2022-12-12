fc8ea7e055126dba052dc3c05e1bc0b6806c6ec1
ChuYaHsu <t199511137@gmail.com>
2022-12-12 11:52:53 +0800

    logics

---
+++ practice/20221212-05-logics.php
@@ -0,0 +1,14 @@
+<?php
+$a = true;
+$b = false;
+
+$c = 5 || 7;
+$d = 5 && 7;
+
+echo "---{$a}---<br>";
+echo "---{$b}---<br>";
+
+var_dump($c);  //查看變數的類型和變數值
+echo "<br>";
+var_dump($d);
+echo "<br>";
