50120e210abe6a4c6233b877c3633a1c68c3a5af
ChuYaHsu <t199511137@gmail.com>
2022-12-13 13:52:43 +0800

    application/json

--- practice/20221213-10-post-data.php
+++ practice/20221213-10-post-data.php
@@ -1,3 +1,8 @@
 <?php
 
-echo json_encode($_POST, JSON_UNESCAPED_UNICODE);
+//伺服器回應給用戶端的擋頭
+header('Content-Type: application/json');  //json mime type = application/json 
+
+
+
+echo json_encode($_POST, JSON_UNESCAPED_UNICODE); //編碼成json字串
