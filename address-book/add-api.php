b8082618b2de787feb3e2c6d4b69e31ce97bb259
ChuYaHsu <t199511137@gmail.com>
2022-12-15 14:37:41 +0800

    日期問題

--- address-book/add-api.php
+++ address-book/add-api.php
@@ -1,37 +1,49 @@
 <?php
 require './parts/connect_db.php';
-header('Content-Type:application/json');
+header('Content-Type: application/json');
 
 $output = [
   'success' => false,
-  'postData' => $_POST,  //回傳值
-  'code' => 0,   //code ->
-  'erros' => []
+  'postData' => $_POST,
+  'code' => 0,
+  'errors' => []
 ];
 
-if (empty($_POST['name'])) {    //如果是空的
-  $output['erros']['name'] = '沒有姓名資料';
+if (empty($_POST['name'])) {
+  $output['errors']['name'] = '沒有姓名資料';
   echo json_encode($output, JSON_UNESCAPED_UNICODE);
   exit;
 }
 
-//TODO: 欄位資料檢查
-$sql = "INSERT INTO `address_book`(`sid`, `name`, `email`, `mobile`, `birthday`, `address`, `created_at`) VALUES(
-  ?,?,?,
-  ?,?, NOW()
-)";
+// TODO: 欄位資料檢查
+
+$sql = "INSERT INTO `address_book`(
+  `name`, `email`, `mobile`, 
+  `birthday`, `address`, `created_at`
+  ) VALUES (
+    ?,?,?,
+    ?,?, NOW()
+  )";
+
 $stmt = $pdo->prepare($sql);
+
+
+$birthday = null;
+if (!empty($_POST['birthday'])) {
+  $t = strtotime($_POST['birthday']);
+  if ($t !== null) {
+    $birthday = date('Y-m-d', $t);
+  }
+}
+
 $stmt->execute([
   $_POST['name'],
   $_POST['email'],
   $_POST['mobile'],
-  $_POST['birthday'],
+  $birthday,
   $_POST['address'],
 ]);
 
 $output['success'] = !!$stmt->rowCount();
-
-
-
 
 echo json_encode($output, JSON_UNESCAPED_UNICODE);
