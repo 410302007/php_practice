c70c92f5f44fa3d1b0db0fe6523e8f570437deaf
ChuYaHsu <t199511137@gmail.com>
2022-12-15 14:15:22 +0800

    add-api 2

--- address-book/add-api.php
+++ address-book/add-api.php
@@ -16,5 +16,22 @@ if (empty($_POST['name'])) {    //如果是空的
 }
 
 //TODO: 欄位資料檢查
+$sql = "INSERT INTO `address_book`(`sid`, `name`, `email`, `mobile`, `birthday`, `address`, `created_at`) VALUES(
+  ?,?,?,
+  ?,?, NOW()
+)";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([
+  $_POST['name'],
+  $_POST['email'],
+  $_POST['mobile'],
+  $_POST['birthday'],
+  $_POST['address'],
+]);
+
+$output['success'] = !!$stmt->rowCount();
+
+
+
 
 echo json_encode($output, JSON_UNESCAPED_UNICODE);
