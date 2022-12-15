c97cc82227808cf38be9f0e854017760d43a6718
ChuYaHsu <t199511137@gmail.com>
2022-12-15 16:58:47 +0800

    表單呈現錯誤訊息

--- address-book/add.php
+++ address-book/add.php
@@ -1,7 +1,8 @@
 <?php
 require './parts/connect_db.php';
 $pageName = 'add';
-$title = '新增資料';
+$title = "新增資料";
+
 
 
 ?>
@@ -14,7 +15,6 @@ $title = '新增資料';
         <div class="card-body">
           <h5 class="card-title">新增資料</h5>
           <form name="form1" onsubmit="checkForm(event)" novalidate>
-            <!--novalidate->不要驗證-->
             <div class="mb-3">
               <label for="name" class="form-label">name</label>
               <input type="text" class="form-control" id="name" name="name" required>
@@ -23,19 +23,19 @@ $title = '新增資料';
 
             <div class="mb-3">
               <label for="email" class="form-label">email</label>
-              <input type="text" class="form-control" id="email" name="email">
+              <input type="email" class="form-control" id="email" name="email">
               <div class="form-text"></div>
             </div>
 
             <div class="mb-3">
               <label for="mobile" class="form-label">mobile</label>
-              <input type="text" class="form-control" id="mobile" name="mobile" pattern="09\d{2}-?\d{3}-\d{3}">
+              <input type="number" class="form-control" id="mobile" name="mobile" pattern="09\d{2}-?\d{3}-?\d{3}">
               <div class="form-text"></div>
             </div>
 
             <div class="mb-3">
               <label for="birthday" class="form-label">birthday</label>
-              <input type="datetime-local" class="form-control" id="birthday" name="birthday">
+              <input type="date" class="form-control" id="birthday" name="birthday">
               <div class="form-text"></div>
             </div>
 
@@ -62,20 +62,54 @@ $title = '新增資料';
 <?php include './parts/scripts.php' ?>
 <script>
   const checkForm = (e) => {
-    e.preventDefault(); //不讓表單送出
+    e.preventDefault(); // 不要讓原來的表單送出
+
+    // 所有輸入欄回復原來的外觀
+    const inputs = document.querySelectorAll('input.form-control');
+    inputs.forEach((el) => {
+      el.style.border = '1px solid #CCCCCC';
+      el.nextElementSibling.innerHTML = '';
+    });
+
 
     // TODO: 欄位資料檢查
 
-    const fd = new FormData(document.form1); //表單物件 //不刷新:不跳轉頁面
-
-    fetch('add-api.php', { //fetch 一定使用2個then
+    const fd = new FormData(document.form1);
+    /*
+        fetch('add-api.php', {
+          method: 'POST',
+          body: fd
+        }).then(function(response){
+          return response.json()
+        }).then(obj=>{
+          console.log(obj);
+        })
+    */
+    fetch('add-api.php', {
         method: 'POST',
         body: fd
-      }).then(r => r.json()) //轉為json文件
+      })
+      .then(r => r.json())
       .then(obj => {
         console.log(obj);
-      })
+        if (obj.success) {
+          alert('新增成功');
+        } else {
+          for (let k in obj.errors) {
+            const el = document.querySelector('#' + k);
+            console.log(el);
+            if (el) {
+              el.style.border = '2px solid red';
+              el.nextElementSibling.innerHTML = obj.errors[k];
+            }
+          }
 
+
+
+        }
+
+
+      })
 
   };
 </script>
