1c46ba8bd9ebc9fd1135e43ba562033df97656ba
ChuYaHsu <t199511137@gmail.com>
2022-12-15 11:25:25 +0800

    當前的頁碼active

--- address-book/list.php
+++ address-book/list.php
@@ -1,21 +1,37 @@
 <?php
 require './parts/connect_db.php';
 
-$perPage = 10;
+$perPage = 25; //每頁25筆
 $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
 if ($page < 1) {
   header('Location: ?page=1');   //location 後可加網址 或路徑(+ /.)  
   exit;
 }
+$t_sql = "SELECT COUNT(1) FROM address_book";   //4000筆
+//總筆數
+$totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];    //fetch_num->索引式陣列 //第一個column就是總筆數
 
-$sql = sprintf(
-  "SELECT * FROM address_book ORDER BY sid DESC LIMIT %s, %s",   //DESC -> 遞減
-  ($page - 1) * $perPage,
-  $perPage
-);
+//總頁數
+$totalPages = ceil($totalRows / $perPage); //ceil  
 
-$rows = $pdo->query($sql)->fetchAll();
+$rows = [];
+//如果有資料的話
+if (!empty($totalRows)) {
+  if ($page > $totalPages) {
+    //頁碼超出範圍，轉向到最後一頁
+    header('Location: ?page = ' . $totalPages);
+    exit;
+  }
 
+
+  $sql = sprintf(
+    "SELECT * FROM address_book ORDER BY sid DESC LIMIT %s, %s",   //DESC -> 遞減
+    ($page - 1) * $perPage,
+    $perPage
+  );
+
+  $rows = $pdo->query($sql)->fetchAll();
+}
 
 
 ?>
@@ -24,6 +40,23 @@ $rows = $pdo->query($sql)->fetchAll();
 <div class="container">
   <div class="row">
     <div class="col">
+      <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item <?= $page == 1 ? 'disabled' : '' ?>">
+            <a class="page-link" href="?page=<?= $page - 1 ?>">Previous</a>
+          </li>
+
+          <?php for ($i = 1; $i <= $totalPages; $i++) : ?>
+            <li class="page-item <?= $i == $page ? 'active' : '' ?> ">
+              <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
+            </li>
+          <?php endfor ?>
+
+          <li class="page-item <?= $page == $totalPages ? 'disabled' : '' ?>">
+            <a class="page-link" href="?page=<?= $page + 1 ?>">Next</a>
+          </li>
+        </ul>
+      </nav>
       <table class="table table-striped table-bordered">
         <thead>
           <tr>
@@ -49,6 +82,7 @@ $rows = $pdo->query($sql)->fetchAll();
         </tbody>
 
       </table>
+
     </div>
   </div>
 </div>
