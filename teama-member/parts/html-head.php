74f150a932d4451f2b811e0af0a82d574f0394b6
ChuYaHsu <t199511137@gmail.com>
2022-12-18 14:08:07 +0800

    步驟一

---
+++ teama-member/parts/html-head.php
@@ -0,0 +1,22 @@
+<?php
+if (isset($title)) {
+  $title = $title . '-Nancy的網站';
+} else {
+  $title = 'Nancy的網站';
+}
+$pageName = $pageName ?? ''; //如果沒有設定，就設定為空字串
+?>
+<!DOCTYPE html>
+<html lang="zh">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <title><?= $title ?></title>
+</head>
+
+<body>
