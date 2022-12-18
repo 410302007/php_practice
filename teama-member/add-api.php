7254c3e258451e14c70d6b498a17bcd45806bc8c
ChuYaHsu <t199511137@gmail.com>
2022-12-19 01:30:23 +0800

    表單的後端驗證

--- teama-member/add-api.php
+++ teama-member/add-api.php
@@ -17,11 +17,27 @@ if (empty($_POST['name'])) {
 }
 
 //TODO: 欄位資料檢查
+$isPass = true; //是否通過驗證(預設:true)
+
+
+
+if (mb_strlen($_POST['name']) < 2) { //mb-strlen->中文字英文字轉字串
+  $output['errors']['name'] = '請填寫正確的姓名';
+  $isPass = false;
+}
+if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
+  $output['errors']['email'] = '請填寫正確的email';
+  $isPass = false;
+}
+
+
+
 $sql = "INSERT INTO `address_book`(`name`, `email`, `mobile`, `birthday`, `address`, `created_at`) VALUES (
   ?,?,?,
   ?,?,NOW()
 )";
 $stmt = $pdo->prepare($sql);
+
 
 $t = strtotime($_POST['birthday']);
 $birthday = null;
@@ -31,16 +47,20 @@ if (!empty($_POST['birthday'])) {
 if ($t !== false) {
   $birthday = date('Y-m-d', $t);
 }
+if ($isPass) {
+  $stmt->execute([
+    $_POST['name'],
+    $_POST['email'],   //$_POST['email']??'', > 如果沒有自然define就寫入空字串
+    $_POST['mobile'] ?? '',
+    $birthday,
+    $_POST['address'] ?? '',
+  ]);
+  //有無新增成功-> 看rowCount
+  $output['success'] = !!$stmt->rowCount();
+}
 
-$stmt->execute([
-  $_POST['name'],
-  $_POST['email'],   //$_POST['email']??'', > 如果沒有自然define就寫入空字串
-  $_POST['mobile'],
-  $_POST['birthday'],
-  $_POST['address'],
-]);
-//有無新增成功-> 看rowCount
-$output['success'] = !!$stmt->rowCount();
+
+
 
 
 echo json_encode($output, JSON_UNESCAPED_UNICODE);
