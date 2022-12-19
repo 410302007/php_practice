ff09c8d0471f095733fdd30fe17cd0853ef7728f
ChuYaHsu <t199511137@gmail.com>
2022-12-19 17:09:55 +0800

    AJAX 上傳, 並取得檔名

--- practice/20221219-13-ajax-upload.php
+++ practice/20221219-13-ajax-upload.php
@@ -2,16 +2,15 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Document</title>
 </head>
 
 <body>
   <form name="form2" onsubmit="return false">
     <input type="text" name="my_img_file" readonly>
-    <!--readonly => 只能讀取-->
     <button type="button" onclick="f.click()">選取檔案並上傳</button>
     <img src="" alt="" id="myimg" width="300" />
 
@@ -19,34 +18,32 @@
     <input type="submit">
   </form>
 
-  <form name="form1" onsubmit="return false" style="display:none;">
-    <input type="file" name="photo" accept="image/*">
-    <br>
+
+  <form name="form1" onsubmit="return false" style="display: none;">
+    <input type="file" name="avatar" accept="image/*" />
   </form>
 
 
   <script>
-    const f = document.form1.photo;
+    const f = document.form1.avatar;
     const myimg = document.querySelector("#myimg");
     f.onchange = async () => {
-      const formData = new FormData(document.form1);
+      const fd = new FormData(document.form1);
       const r = await fetch('upload-single.php', {
         method: 'POST',
-        body: formData
-
+        body: fd
       });
       const data = await r.json();
       console.log({
         data
       });
       if (data.success) {
-        //上傳成功
+        // 成功上傳
         myimg.src = './../uploads/' + data.filename;
         document.form2.my_img_file.value = data.filename;
       } else {
         alert(data.error || '沒有上傳的檔案');
       }
-
     };
   </script>
 </body>
