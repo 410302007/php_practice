35a598cb7b90d0a6d79984ede99f02c2d70ecdb8
ChuYaHsu <t199511137@gmail.com>
2022-12-24 02:27:42 +0800

    更改轉畫面(list3)

--- teama-member/parts/navbar.php
+++ teama-member/parts/navbar.php
@@ -16,7 +16,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link <?= $pageName == 'list' ? 'active' : '' ?>" href="list.php">列表</a>
+            <a class="nav-link <?= $pageName == 'list' ? 'active' : '' ?>" href="list3.php">列表</a>
           </li>
           <li class="nav-item">
             <a class="nav-link <?= $pageName == 'add' ? 'active' : '' ?>" href="add.php">新增</a>
