9729c8c1f0c39d574ab6254fa65fd98e59585b7c
ChuYaHsu <t199511137@gmail.com>
2022-12-24 15:01:08 +0800

    更改頁面

--- teama-member/welcome.php
+++ teama-member/welcome.php
@@ -24,7 +24,7 @@ $row = $stmt->fetch();
 <h1 class="my-5">你好， <b><?php echo $_SESSION["client"]['name']; ?></b>，歡迎光臨!</h1>
 <p>
   <a href="edit-for-client.php?mid=<?php echo $_SESSION["client"]['mid']; ?>" class="btn btn-primary">編輯個人資料</a>
-  <a href="reset-password.php" class="btn btn-warning">重設密碼</a>
+  <!-- <a href="reset-password.php" class="btn btn-warning">重設密碼</a> -->
   <a href="logout-api.php" class="btn btn-danger ml-3">登出</a>
 </p>
 <?php include './parts/scripts.php' ?>
