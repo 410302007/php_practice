8f647b3f838084c1b903979758ba9496324baf96
ChuYaHsu <t199511137@gmail.com>
2022-12-24 14:59:42 +0800

    delete.php (fix bug)

--- teama-member/delete.php
+++ teama-member/delete.php
@@ -2,17 +2,17 @@
 require './admin-required.php';
 require './parts/connect_db.php';
 
-$sid =  isset($_GET['mid']) ? intval($_GET['mid']) : 0;
+$mid =  isset($_GET['mid']) ? intval($_GET['mid']) : 0;
 
-if (empty($sid)) {
-  header('Location: list.php');  //回到列表頁
+if (empty($mid)) {
+  header('Location: list3.php');  //回到列表頁
   exit;
 }
-$sql = "DELETE FROM `member` WHERE mid=$mid";
+$sql = "DELETE FROM `member` WHERE mid =$mid";
 $pdo->query($sql);
 
 if (empty($_SERVER['HTTP_REFERER'])) {
-  $come_from = 'list.php';
+  $come_from = 'list3.php';
 } else {
   $come_from = $_SERVER['HTTP_REFERER']; // 從哪裡來, 回哪裡去
 }
